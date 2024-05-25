b1a015aeec3eb49d5f7c2e5809b34b148323ea13
Pedro Almeida <pedroh_7@hotmail.com>
2024-05-25 20:44:39 -0300

    Rework already authenticated callback

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
-const { pbkdf2Sync } = require('node:crypto');
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const { pbkdf2Sync } = require("node:crypto");
 
-const instrutorRoutes = require('./routes/instructorRoutes');
-const adminRoutes = require('./routes/adminRoutes');
-const courseRoutes = require('./routes/courseRoutes');
-const studentRoutes = require('./routes/studentRoutes');
+const instrutorRoutes = require("./routes/instructorRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+const courseRoutes = require("./routes/courseRoutes");
+const studentRoutes = require("./routes/studentRoutes");
 
 const db = require("./db");
 
@@ -17,124 +17,168 @@ const PORT = 3000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use('/instructor', instrutorRoutes);
-app.use('/admin', adminRoutes);
-app.use('/course', courseRoutes);
-app.use('/student', studentRoutes);
+app.use("/instructor", instrutorRoutes);
+app.use("/admin", adminRoutes);
+app.use("/course", courseRoutes);
+app.use("/student", studentRoutes);
 
 app.get("/", (req, res) => {
-  if(req.cookies.token) {
-    const { iat, ...user } = jwt.verify(req.cookies.token, 'jkey');
-    return res.json({
-      auth: 1,
-      user: user,
+  if (req.cookies.token) {
+    const { iat, ...user } = jwt.verify(req.cookies.token, "jkey");
+
+    const q = "SELECT * FROM usu_usuario WHERE usu_id = ?";
+    db.query(q, user.id, (err, result) => {
+      if (err) {
+        res.statusCode = 400;
+        return res.json(err);
+      }
+
+      if (!result[0]) {
+        res.statusCode = 400;
+        return res.json();
+      }
+
+      function usrType() {
+        switch (result[0]["usu_tipo"]) {
+          case "estudante":
+            return {
+              id: result[0]["usu_id"],
+              email: result[0]["usu_email"],
+              nome: result[0]["usu_nome"],
+              tipo: result[0]["usu_tipo"],
+              experiencia: result[0]["usu_experiencia"],
+              nivel: result[0]["usu_nivel"],
+            };
+
+          case "instrutor":
+            return {
+              id: result[0]["usu_id"],
+              email: result[0]["usu_email"],
+              nome: result[0]["usu_nome"],
+              tipo: result[0]["usu_tipo"],
+            };
+
+          case "admin":
+            return {
+              id: result[0]["usu_id"],
+              email: result[0]["usu_email"],
+              nome: result[0]["usu_nome"],
+              tipo: result[0]["usu_tipo"],
+            };
+        }
+      }
+
+      const user = usrType();
+
+      return res.json({
+        auth: 1,
+        user: user,
+      });
     });
+  } else {
+    return res.json({ auth: 0 });
   }
-  res.json({auth: 0,});
 });
 
 app.post("/signup", (req, res) => {
+  const hash = pbkdf2Sync(req.body.senha, "10", 1000, 32, "sha512").toString(
+    "hex"
+  );
 
-  const hash = pbkdf2Sync(req.body.senha, '10', 1000, 32, 'sha512').toString('hex');
-  
-  const values = [
-    req.body.nome,
-    req.body.email,
-    hash,
-    req.body.tipo
-  ]
+  const values = [req.body.nome, req.body.email, hash, req.body.tipo];
 
-  const q = "INSERT INTO usu_usuario (`usu_nome`, `usu_email`, `usu_senha`, `usu_tipo`) VALUES (?)";
+  const q =
+    "INSERT INTO usu_usuario (`usu_nome`, `usu_email`, `usu_senha`, `usu_tipo`) VALUES (?)";
   db.query(q, [values], (err, result) => {
-    if(err) {
+    if (err) {
       res.statusCode = 400;
       return res.json(err);
     }
 
     return res.json(result);
-  })
+  });
 });
 
-
-app.post('/login', (req, res) => {
-
+app.post("/login", (req, res) => {
   const q = "SELECT * FROM usu_usuario WHERE usu_email = ?";
   db.query(q, req.body.email, (err, result) => {
-    if(err) {
+    if (err) {
       res.statusCode = 400;
       return res.json(err);
     }
 
-    if(!result[0]) {
+    if (!result[0]) {
       res.statusCode = 400;
       return res.json();
     }
 
     function usrType() {
-      switch(result[0]['usu_tipo']) {
-        case 'estudante':
+      switch (result[0]["usu_tipo"]) {
+        case "estudante":
           return {
-            id: result[0]['usu_id'],
-            email: result[0]['usu_email'],
-            nome: result[0]['usu_nome'],
-            tipo: result[0]['usu_tipo'],
-            experiencia: result[0]['usu_experiencia'],
-            nivel: result[0]['usu_nivel']
-          }
-  
-        case 'instrutor':
+            id: result[0]["usu_id"],
+            email: result[0]["usu_email"],
+            nome: result[0]["usu_nome"],
+            tipo: result[0]["usu_tipo"],
+            experiencia: result[0]["usu_experiencia"],
+            nivel: result[0]["usu_nivel"],
+          };
+
+        case "instrutor":
           return {
-            id: result[0]['usu_id'],
-            email: result[0]['usu_email'],
-            nome: result[0]['usu_nome'],
-            tipo: result[0]['usu_tipo']
-          }
-  
-        case 'admin':
+            id: result[0]["usu_id"],
+            email: result[0]["usu_email"],
+            nome: result[0]["usu_nome"],
+            tipo: result[0]["usu_tipo"],
+          };
+
+        case "admin":
           return {
-            id: result[0]['usu_id'],
-            email: result[0]['usu_email'],
-            nome: result[0]['usu_nome'],
-            tipo: result[0]['usu_tipo']
-          }
+            id: result[0]["usu_id"],
+            email: result[0]["usu_email"],
+            nome: result[0]["usu_nome"],
+            tipo: result[0]["usu_tipo"],
+          };
       }
     }
 
+    const hash = pbkdf2Sync(req.body.senha, "10", 1000, 32, "sha512").toString(
+      "hex"
+    );
 
-    const hash = pbkdf2Sync(req.body.senha, '10', 1000, 32, 'sha512').toString('hex');
-
-    if(hash !== result[0]['usu_senha']) {
+    if (hash !== result[0]["usu_senha"]) {
       res.statusCode = 400;
       return res.json();
     }
 
     const user = usrType();
 
-    const token = jwt.sign({
-      id: user.id,
-      tipo: user.tipo,
-    }, 'jkey');
+    const token = jwt.sign(
+      {
+        id: user.id,
+        tipo: user.tipo,
+      },
+      "jkey"
+    );
 
-    res.cookie('token', token);
+    res.cookie("token", token);
 
     return res.json(user);
-
-  })
-
+  });
 });
 
-app.get('/logout', (req, res) => {
+app.get("/logout", (req, res) => {
   res.clearCookie("token");
 
-  return res.json('Deslogado com sucesso!')
-})
-
-
+  return res.json("Deslogado com sucesso!");
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
